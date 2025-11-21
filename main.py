94fb9dc63aa6b3cbeaa8533fab70c647989fe934
PolyphasicBlob <244575527+PolyphasicBlob@users.noreply.github.com>
2025-11-20 19:29:23 -0700

    added basic wordcount function

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
 
-    print(file_contents)
-
     return file_contents
 
 
+def count_words(file_contents):
+    wordcount = len(file_contents.split())
+    print(wordcount)
+    return wordcount
+
+
 def main():
-    get_book_text("books/frankenstein.txt")
+    return count_words(get_book_text("books/frankenstein.txt"))
 
 
 main()
