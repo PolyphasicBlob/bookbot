473ea6fb3041893b923101f4c9f592ff3305565a
PolyphasicBlob <244575527+PolyphasicBlob@users.noreply.github.com>
2025-11-20 21:27:54 -0700

    prints formatted report, still needs character count sort

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from stats import count_characters, count_words
 
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     words_counted = count_words(get_book_text("books/frankenstein.txt"))
     print(f"Found {words_counted} total words")
-
+    print("--------- Character Count -------")
     characters_counted = count_characters(get_book_text("books/frankenstein.txt"))
     print(characters_counted)
 
--- stats.py
+++ stats.py
@@ -5,14 +5,14 @@ def count_words(file_contents):
 
 def count_characters(file_contents):
     characount = {}
-
     lowercase_file_contents = file_contents.lower()
-
     for character in lowercase_file_contents:
         if character in characount:
             characount[character] += 1
-
         else:
             characount[character] = 1
-
     return characount
+
+
+def sort_chara_dictionary(characount):
+    pass
