b96d50b627efdef5b271b1e3d5a6e7507e565130
PolyphasicBlob <244575527+PolyphasicBlob@users.noreply.github.com>
2025-11-21 13:11:03 -0700

    removed hardcoding, now accepts file paths in CLI

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if len(sys.argv) <= 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import count_characters, count_words, sort_chara_dictionary
 
 
@@ -10,14 +16,12 @@ def get_book_text(path_to_file):
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    words_counted = count_words(get_book_text("books/frankenstein.txt"))
+    words_counted = count_words(get_book_text(sys.argv[1]))
     print(f"Found {words_counted} total words")
     print("--------- Character Count -------")
-    final_count = sort_chara_dictionary(
-        count_characters(get_book_text("books/frankenstein.txt"))
-    )
+    final_count = sort_chara_dictionary(count_characters(get_book_text(sys.argv[1])))
     for letter in final_count:
         if letter["char"].isalpha():
             print(f"{letter['char']}: {letter['num']}")
